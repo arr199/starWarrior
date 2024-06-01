98de0988d8836aa4e05a7f5525368d55f81fbf63
Abiel Rodriguez <abiel.rodriguez@volenday.com>
2024-06-01 16:44:27 +0100

    chore: Add initial project files and configurations

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { useOnPlayerMove } from "./hooks/useOnPlayerMove";
+import { Header } from "./components/header";
+import { useOnPlayerShoot } from "./hooks/useOnPlayerShoot";
+
+function App() {
+  const player = React.useRef<HTMLDivElement>(null);
+  const gameContainer = React.useRef<HTMLDivElement>(null);
+  const [playerPosition, setPlayerPosition] = useState<PlayerPosition>({
+    bottom: 0,
+    left: 0,
+  });
+
+  useOnPlayerMove(setPlayerPosition, player, gameContainer);
+  useOnPlayerShoot();
+
+  return (
+    <div className="min-h-[100vh] flex flex-col items-center">
+      <Header></Header>
+      <main className="flex flex-col w-full h-full items-center">
+        {/* GAME CONTAINER */}
+        <section
+          ref={gameContainer}
+          className="relative mt-4 border-2 border-white w-[500px] aspect-[3/4] "
+        >
+          {/* PLAYER */}
+          <div
+            ref={player}
+            style={{ bottom: playerPosition.bottom, left: playerPosition.left }}
+            className="absolute w-8 h-8 bg-red-500 bottom-0 left-0"
+          ></div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
