237d029a0a1340db4fa401b6b90697c02629d2ac
Abiel Rodriguez <abiel.rodriguez@volenday.com>
2024-06-01 16:45:17 +0100

    Add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="p-4 bg-blue-400 w-full flex justify-center items-center">
+      <h1 className="text-3xl font-bold">Star Warrior</h1>
+    </header>
+  );
+}
